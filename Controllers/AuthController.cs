8f4be20bb23baa79aaf2424acae5bffb170e9e0d
SM <m@user.com>
2025-03-22 11:54:35 +0600

    initial

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OstadAssignment2.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string txtUserNm, string txtPassword)
+        {
+            return View();
+        }
+
+        public ActionResult ForgetPass()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/LoginUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OstadAssignment2.Models
+{
+    public class LoginUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+
+      
+    }
+}
